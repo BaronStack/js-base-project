9605f46caa57020905d2b510a8022bfcf75929cb
hosts <BaronStack@users.noreply.github.com>
2017-08-15 01:11:23 +0800

    FINISHED

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -7,25 +7,34 @@ var expect = chai.expect;
 chai.use(sinonChai);
 
 var main = require("../lib/main.js");
-
+var lyrics = require('../lib/lyrics');
 
 describe("测试描述", function(){
     sinon.spy(console, 'log');
 
-    it("测试用例1", function(){
-
-        var result = main();
-        var expect_string = '';
+    it("TEST1--输入数字为0，获得最后一段歌词", function(){
+        var input = 0;
+        var result = main(input);
+        var expect_string = lyrics.lastLyrics();
         
         expect(expect_string).to.equal(result);
     });
 
-    it("测试用例2", function(){
+    it("TEST2--输入数字为99，获得全部歌词", function(){
 
-        main();
-        var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
+        var input = 99;
+        var result = main(input);
+        var expect_string = lyrics.entireLyrics();
 
         expect(expect_string).to.equal(result);
     });
+    it("TEST3--输入数字1，获得最后两段歌词", function(){
+        var input = 1;
+        var result = main(input);
+        var expect_string = lyrics.lastTwoLyrics();
+
+        expect(expect_string).to.equal(result);
+    });
+
+
 });
